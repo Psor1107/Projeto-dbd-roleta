b10e178f0f920ce1e1c9fa81a27af02154413f4f
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2022-01-21 13:04:51 -0300

    salvando arquivo JSON
    - Coletando os nomes dos perks e salvando tudo em JSON

--- scrapping.py
+++ scrapping.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-from matplotlib.pyplot import table
+import json
 
 
 
@@ -11,12 +11,19 @@ perks_tables = soup.findAll('table', class_='wikitable unknownClass')
 survivors_perks_table = perks_tables[0]
 killers_perks_table = perks_tables[1]
 
+survivors_perks_list = []
 for table_row in survivors_perks_table.findAll('tr')[1:]:
+    perk = {}
     row_headers = table_row.findAll('th')
     icon_field = row_headers[0]
     name_field = row_headers[1]
     description_field = table_row.find('td')
     character_field = row_headers[2]
+    perk['name'] = name_field.a.text
 
+    
+    survivors_perks_list.append(perk)
 
-    print(f"{character_field}\n--------------------------\n")
+print(survivors_perks_list)
+with open('data/teste.json', 'w') as surv_perks_file:
+    json.dump(survivors_perks_list, surv_perks_file)
