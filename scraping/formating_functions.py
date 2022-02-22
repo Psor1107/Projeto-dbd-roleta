ea147b1109e284b9732a18af696a4d112f0ec966
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2022-02-21 21:03:14 -0300

    bug fix (% nao retirado da formatação)

--- scraping/formating_functions.py
+++ scraping/formating_functions.py
@@ -2,5 +2,5 @@ import re
 import unidecode
 
 def format_name(name):
-    name = re.sub('[\s!?:"]', "", name.title())
+    name = re.sub('[\s!?%:"]', "", name.title())
     return unidecode.unidecode(name)
